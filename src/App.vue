558b51de3b34dd9716b3d9dec84ab0709c6e4f03
서인석 <seois830926@inzent.com>
2019-05-21 14:40:07 +0900

    first import vuex

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    Parent counter : {{ count }} <br>
+    <button @click="addCounter">+</button>
+    <button @click="subCounter">-</button>
+
+    <!-- Child 컴포넌트를 등록하고 counter 데이터 속성을 props 로 전달한다. -->
+    <child v-bind:passedCounter="counter"></child>
+  </div>
+</template>
+
+<script>
+  // App.vue
+  import Child from './Child.vue'
+
+  export default {
+    data () {
+      return {
+        // data 속성 등록
+        counter: 0
+      }
+    },
+    methods: {
+      // 이벤트 추가
+      addCounter() {
+        this.counter++;
+      },
+      subCounter() {
+        this.counter--;
+      }
+    },
+    components: {
+      // Child 컴포넌트를 하위 컴포넌트로 등록
+      'child': Child
+    }
+  }
+</script>
---
+++ src/Child.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+  <hr>
+   Child counter : {{ passedCounter }}
+   <br>
+   <button>+</button>
+   <button>-</button>
+  </div>
+</template>
+
+<script>
+  // Child.vue
+  export default {
+    // Parent 에서 넘겨준 counter 속성을 passedCounter 로 받음
+    props: ['passedCounter']
+  }
+</script>
